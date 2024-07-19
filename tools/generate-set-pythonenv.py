c8cbe03715be808c081fb4a67194dac77ac6da9b
puffycodes <as1234dfgh@gmail.com>
2024-07-19 12:09:04 +0800

    add a script to generate shell script for setting python environment variables

--- tools/generate-set-pythonenv.py
+++ tools/generate-set-pythonenv.py
@@ -26,7 +26,37 @@ export PYTHONPATH={path_value}
     return
 
 def mismatch_warning(script_type, os_name, ferr=sys.stderr):
-    print(f'Warning: script type ({script_type}) generated on this os ({os_name}) may not work', file=ferr)
+    print(
+        f'Warning: script type ({script_type}) generated on this os ({os_name}) may not work correctly',
+        file=ferr
+    )
+    return
+
+def generate_script(path_list, script_type, fout=sys.stdout, ferr=sys.stderr):
+    abs_path_list = []
+    for path in path_list:
+        abs_path = os.path.abspath(os.path.join(path))
+        abs_path_list.append(abs_path)
+
+    os_name = os.name
+    if script_type == 'windows':
+        if os_name != 'nt':
+            mismatch_warning(script_type, os_name)
+        generate_windows_script(abs_path_list, fout=fout)
+    elif script_type ==  'posix':
+        if os_name != 'posix' and os_name != 'java':
+            mismatch_warning(script_type, os_name)
+        generate_posix_script(abs_path_list, fout=fout)
+    elif script_type == None or script_type == '' or script_type == 'auto':
+        if os_name == 'posix' or os_name == 'java':
+            generate_posix_script(abs_path_list, fout=fout)
+        elif os_name == 'nt':
+            generate_windows_script(abs_path_list, fout=fout)
+        else:
+            print(f'Error: unknown os name: {os_name}', file=ferr)
+    else:
+        print(f'Error: unknown script type: {script_type}', file=ferr)
+
     return
 
 def main():
@@ -43,30 +73,7 @@ def main():
 
     fout = sys.stdout
     ferr = sys.stderr
-
-    path_list = []
-    for path in args.pathname:
-        full_path = os.path.abspath(os.path.join(path))
-        path_list.append(full_path)
-
-    os_name = os.name
-    if args.script_type == 'windows':
-        if os_name != 'nt':
-            mismatch_warning(args.script_type, os_name)
-        generate_windows_script(path_list, fout=fout)
-    elif args.script_type ==  'posix':
-        if os_name != 'posix' and os_name != 'java':
-            mismatch_warning(args.script_type, os_name)
-        generate_posix_script(path_list, fout=fout)
-    elif args.script_type == None or args.script_type == '' or args.script_type == 'auto':
-        if os_name == 'posix' or os_name == 'java':
-            generate_posix_script(path_list, fout=fout)
-        elif os_name == 'nt':
-            generate_windows_script(path_list, fout=fout)
-        else:
-            print(f'Error: unknown os name: {os_name}', file=ferr)
-    else:
-        print(f'Error: unknown script type: {args.script_type}', file=ferr)
+    generate_script(args.pathname, args.script_type, fout=fout, ferr=ferr)
 
     return
 
