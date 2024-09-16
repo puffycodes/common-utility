d3dd2d62a12bb877c2dd4c0bdb3011ef692c357c
puffycodes <as1234dfgh@gmail.com>
2024-09-16 11:18:33 +0800

    add test for dir utility

---
+++ tests/dir_util_test.py
@@ -0,0 +1,15 @@
+# file: dir_util_test.py
+
+import unittest
+from common_util.dir_util import DirectoryUtility
+
+class DirectoryUtilityTest(unittest.TestCase):
+
+    def test_01(self):
+        print(f'no test here yet')
+        return
+    
+if __name__ == '__main__':
+    unittest.main()
+
+# --- end of file --- #
