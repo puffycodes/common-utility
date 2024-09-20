8556ef3b0f5cd3bdecf45c80a7490c9078498f07
puffycodes <as1234dfgh@gmail.com>
2024-09-20 16:13:15 +0800

    add documentation generation using Sphinx

--- docs/sphinx-docs/source/conf.py
+++ docs/sphinx-docs/source/conf.py
@@ -30,10 +30,11 @@ extensions = [
 templates_path = ['_templates']
 exclude_patterns = []
 
-
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'alabaster'
 html_static_path = ['_static']
+
+# -- Options for EPUB output -------------------------------------------------
+epub_show_urls = 'footnote'
